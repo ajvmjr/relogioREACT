413698b60e02d46e6910969c1e39b4184a299f91
FIC <FIC@info.local>
2019-11-13 12:07:26 -0200

    criação de relógio com react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function DataFormatada(props){
+  return <h2>Horário atual: {props.date.toLocaleTimeString()}</h2>
+}
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  //Ciclo de vida que ocorre quando o componente é inserido no DOM
+  //Através da função setInterval, o relógio é criado com o timerID atelado
+  //Chama a função thick() a cada 1 segundo
+  componentDidMount(){
+    this.timerId = setInterval(() => {
+      this.thick()
+    }, 1000)
+
+    console.log('Eu sou o relógio: ' + this.timerId);
+  }
+
+  //Ciclo de vida que ocorre quando o componente é removida
+  componentWillUnmount(){
+    clearInterval(this.timerId);
+  }
+
+  //Define no state date a data atual a cada vez que é chamada
+  thick(){
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  //Renderiza na tela o título e o retorno da função DataFormatada
+  //Passando date como valor atual do state
+  render(){
+    return(
+      <div>
+        <h1>Relógio</h1>
+        <DataFormatada date={this.state.date}/>
+      </div>
+    )
+  }
+}
+
+//Função principal que é chamada no index.js
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Clock/>
+        <Clock/>
       </header>
     </div>
   );
